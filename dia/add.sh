8958a249fe908a8fa061a13784c9b6a008e17db9
ufrs12 <ufrs12@yandex.ru>
2024-05-07 19:16:52 +0300

    1

--- dia/add.sh
+++ dia/add.sh
@@ -1,1 +1,5 @@
 cp -r $(dirname "$(realpath $0)")/ethernet/* $HOME/.dia/
+
+n=$(grep -n 'compress_save' $HOME/.dia/persistence | sed "s/:.*//")
+nn=$((n + 2))
+sed -i "$n,$nn s/true/false/" $HOME/.dia/persistence
